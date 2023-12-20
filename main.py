3ba374fd91d6f5a624a485a2e1408739aa1743b0
Svetozar Kanev <97878954+sh1d0wg1m3r@users.noreply.github.com>
2023-12-20 20:46:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,34 @@ import requests
 import time
 import json
 import configparser
+from fake_useragent import UserAgent
+from requests.exceptions import RequestException
+import random
 
 URL = "https://api.discord.gx.games/v1/direct-fulfillment"
 UID = {"partnerUserId": "69f7b7c20983f391d4ea2726da99f7144828e4f6136308f11b254c2ded43c556"}
 BASE_URL = "https://discord.com/billing/partner-promotions/1180231712274387115/"
 SETTINGS = configparser.ConfigParser()
+ua = UserAgent()
 SETTINGS.read("settings.ini")
 
+# Generate a random User-Agent string
+random_user_agent = ua.random
+
+headers = {
+    "User-Agent": random_user_agent
+}
+
 def getToken() -> str:
-    response = requests.post(URL, json=UID)
+    try:
+        response = requests.post(URL, json=UID, headers=headers)
+        response.raise_for_status()  # Raise an exception for non-2xx responses
 
-    data = json.loads(response.text)
-
-    return data["token"]
+        data = json.loads(response.text)
+        return data["token"]
+    except RequestException as e:
+        print(f"Error: {e}")
+        return None
 
 links = set()
 
@@ -22,9 +37,9 @@ try:
     for number in range(int(SETTINGS["DEFAULT"]["GenerationAmount"])):
         token = getToken()
 
-        links.add(BASE_URL + token)
-
-        print(f"Generated Token #{number + 1}. Press CTRL+C to stop and write links to links.txt.")
+        if token is not None:
+            links.add(BASE_URL + token)
+            print(f"Generated Token #{number + 1}. Press CTRL+C to stop and write links to links.txt.")
         
         time.sleep(int(SETTINGS["DEFAULT"]["Cooldown"]))
 except KeyboardInterrupt:
@@ -33,4 +48,4 @@ except KeyboardInterrupt:
 with open("links.txt", "w") as file:
     file.write("\n".join(links)) # Tokens are written to the links.txt file upon closing the program.
 
-    print("\nLINKS WRITTEN TO links.txt")
+print("\nLINKS WRITTEN TO links.txt")
